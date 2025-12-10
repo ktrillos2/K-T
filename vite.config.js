75cd39ae3a80f5d9a7ecfb016ca10ac6d37deba8
ktrillos <ktrillos2@gmail.com>
2025-12-10 16:56:44 -0500

    Primer commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          spline: ['@splinetool/runtime', '@splinetool/react-spline'],
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+})
