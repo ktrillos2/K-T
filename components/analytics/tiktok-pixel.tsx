fec5aee1ab1cb5b77e31efe749bda9be1e06506a
Keyner <contactoktweb@gmail.com>
2026-07-18 15:56:28 -0500

    analytics: update TikTok Pixel ID

--- components/analytics/tiktok-pixel.tsx
+++ components/analytics/tiktok-pixel.tsx
@@ -60,7 +60,7 @@ export default function TiktokPixel() {
             var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
             ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
           
-            ttq.load('D5PGFD3C77UAU1QU4SH0');
+            ttq.load('D9DUGJRC77U03DOJFV7G');
             ttq.page(); 
           }(window, document, 'ttq');
         `,
