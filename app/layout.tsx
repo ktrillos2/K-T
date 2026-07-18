a43e86d9574fa289c6a30794eb42c4448e007f8e
Keyner <contactoktweb@gmail.com>
2026-07-18 16:04:02 -0500

    analytics: move TikTok pixel directly to head and remove lazy loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ import GoogleAnalytics from "@/components/analytics/google-analytics"
 import GoogleTagManager from "@/components/analytics/google-tag-manager"
 import MetaPixel from "@/components/analytics/meta-pixel"
 import MicrosoftClarity from "@/components/analytics/microsoft-clarity"
-import TiktokPixel from "@/components/analytics/tiktok-pixel"
 import TikTokEventsTracker from "@/components/analytics/tiktok-events-tracker"
 import { Toaster } from "@/components/ui/sonner"
 import { CursorProvider } from "@/context/cursor-context"
@@ -137,6 +136,23 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://api.exchangerate-api.com" />
         <link rel="dns-prefetch" href="https://ipapi.co" />
         <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
+        {/* TikTok Pixel Code Start */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+
+  ttq.load('D9DUGJRC77U03DOJFV7G');
+  ttq.page();
+}(window, document, 'ttq');
+            `,
+          }}
+        />
+        {/* TikTok Pixel Code End */}
       </head>
       <body
         className={`${firaCode.variable} ${vt323.variable} ${pressStart2P.variable} ${ariW9500.variable} font-mono antialiased`}
@@ -172,7 +188,6 @@ export default function RootLayout({
         <SpeedInsights />
         <GoogleAnalytics />
         <React.Suspense fallback={null}>
-          <TiktokPixel />
           <MetaPixel />
         </React.Suspense>
         <TikTokEventsTracker />
